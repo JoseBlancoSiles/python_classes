c80c42b54fb854410fc32d3457d6d4889e21c596
Jose Siles <josesb@ext.inditex.com>
2024-06-19 20:14:40 +0200

    Ejercicio 1 (faltan buenas prácticas, pero la lógica funciona)

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 # Funciones
 
-def is_primo_circular(num):
-    for i in range(2, int(num ** 0.5) + 1) :
+def is_primo(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num ** 0.5) + 1, 2):
         if num % i == 0:
             return False
     return True
 
-print(is_primo_circular(4))
+
+def rotar_numero(num):
+    if not is_primo(num):
+        return False
+    if num < 10:
+        return True
+    
+    s = str(num)
+    for i in range(len(s)):
+        rotacion = (s[i:] + s[:i])
+        if not is_primo(rotacion):
+            return False
+    return True
+
+
 
 
 
