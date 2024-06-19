9843123b65acb492096cb10b5518968a7809aa5c
Jose Siles <josesb@ext.inditex.com>
2024-06-19 23:56:20 +0200

    Ejercicio 1--> buenas prácticas implementadas, código documentado y más modular

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
 # Funciones
 
 def is_primo(num):
-    if num <= 1:
+    '''Función que recibe un número y devuelve True para números primos y false para números no primos'''
+    if num <= 1 or (num > 2 and num % 2 == 0):
         return False
     if num == 2:
         return True
-    if num % 2 == 0:
-        return False
     for i in range(3, int(num ** 0.5) + 1, 2):
         if num % i == 0:
             return False
     return True
 
-
 def rotar_numero(num):
-    if not is_primo(num):
-        return False
-    if num < 10:
-        return True
-    
+    '''Recibe un número y guarda en una lista todas sus rotaciones'''
+    rotaciones = []
     s = str(num)
     for i in range(len(s)):
-        rotacion = (s[i:] + s[:i])
+        rotaciones.append(int(s[i:] + s[:i]))
+    return rotaciones
+
+def is_primo_circular(num):
+    '''Recibe un número y devuelve True si es un primo circular, false si no lo es'''
+    if not is_primo(num):
+        return False
+    for rotacion in rotar_numero(num):
         if not is_primo(rotacion):
             return False
     return True
-
-
-
-
-
-
-
-        
-
