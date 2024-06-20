8ea76b4e6361082654ffcadfc5e109b58086f583
Jose Siles <josesb@ext.inditex.com>
2024-06-20 18:24:13 +0200

    ejercicio 4

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-# FUNCIONES
+# IMPORTS
 
-## FUNCIONES EJERCICIO 1
+import string
+
+## EJERCICIO 1
 
 def is_primo(num):
     '''Función que recibe un número y devuelve True para números primos y false para números no primos'''
@@ -30,7 +32,7 @@ def is_primo_circular(num):
             return False
     return True
 
-## FUNCIONES EJERCICIO 2
+## EJERCICIO 2
 
 def all_numbers_different(num):
     '''Recibe un número y devuelve True si todos los dígitos son diferentes, false si al menos un número se repite'''
@@ -44,7 +46,7 @@ def all_numbers_different(num):
         digitos_vistos.add(digito)
     return True
 
-## FUNCIONES EJERCICIO 3
+## EJERCICIO 3
 
 def text_chain_based_on_digit_sum(num):
     '''Recibe un número, devuelve la última suma válida y la añade a la izquierda del número recibido'''
@@ -60,3 +62,33 @@ def sumar_digitos(num):
     for digito in s:
         suma += int(digito)
     return suma
+
+## EJERCICO 4
+
+import string
+
+class CustomString:
+    def __init__(self, cadena_inicial):
+        self.caracteres_permitidos = set(string.ascii_letters + ' ')
+        self.value = self.filtrar_string(cadena_inicial)
+    
+    def filtrar_string(self, s):
+        '''Filtra los caracteres no permitidos de una cadena, los muestra y devuelve la cadena filtrada'''
+        no_permitidos = []
+        permitido = ''
+        for caracter in s:
+            if caracter not in self.caracteres_permitidos:
+                no_permitidos.append(caracter)
+            else:
+                permitido += caracter
+        if no_permitidos: 
+            print(f'La instancia creada solo puede contener caracteres de la "a" a la "z". Los caracteres: {no_permitidos} se eliminarán')
+        return permitido
+    
+    def __str__(self):
+        return self.value
+    
+    def __add__(self, s):
+        filtrado = self.filtrar_string(s)
+        cadena = self.value + filtrado
+        return CustomString(cadena)
